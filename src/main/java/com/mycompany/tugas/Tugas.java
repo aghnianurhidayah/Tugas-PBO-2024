f530bcda95f46be5086f478af041d4d46744877d
Aghnia Nurhidayah <aghnianurhidayah@gmail.com>
2024-05-30 22:21:55 +0800

    upload tugas 1 pbo

---
+++ src/main/java/com/mycompany/tugas/Tugas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas;
+
+/**
+ *
+ * @author Aghnia
+ */
+public class Tugas {
+
+    public static void main(String[] args) {
+        bookmanagement bm = new bookmanagement();
+        bm.setVisible(true);
+    }
+}
